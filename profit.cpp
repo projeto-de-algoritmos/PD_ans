3e257fef121b323a0f876d8ef48553bb9f3db7ea
Marcus Martins <89209017+marcusmartinss@users.noreply.github.com>
2023-12-14 00:15:04 -0300

    Atualiza profit.cpp
    Refatorando a solução por força bruta para PD

--- profit.cpp
+++ profit.cpp
@@ -2,38 +2,39 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int N;
 
     while (cin >> N)
     {
         int custo_dia; cin >> custo_dia;
 
-        vector<int> revenue(N);
+        vector<int> receita(N);
 
         for (int i = 0; i < N; ++i) {
-            cin >> revenue[i];
+            cin >> receita[i];
         }
 
-        int maxProfit = 0;
+        // Vetor para armazenar o lucro máximo até cada dia
+        vector<int> lucroMaximoAte(N, 0);
 
-        for (int i = 0; i < N; ++i)
+        // Inicialização do lucro máximo até o primeiro dia
+        lucroMaximoAte[0] = max(0, receita[0] - custo_dia);
+        int lucroMaximo = lucroMaximoAte[0];
+
+        // Iteração pelos dias para calcular o lucro máximo até cada dia
+        for (int i = 1; i < N; ++i)
         {
-            for (int j = i; j < N; ++j)
-            {
-                int profit = 0;
-                
-                for (int k = i; k <= j; ++k) {
-                    profit += revenue[k];
-                }
+            // O lucro máximo até o dia i é o máximo entre 0 e o lucro até o dia i-1
+            // somado ao lucro do dia i e subtraído do custo diário
+            lucroMaximoAte[i] = max(0, lucroMaximoAte[i - 1] + receita[i] - custo_dia);
 
-                profit -= (j - i + 1) * custo_dia;
-                maxProfit = max(maxProfit, profit);
-            }
+            // Atualiza o lucro máximo global
+            lucroMaximo = max(lucroMaximo, lucroMaximoAte[i]);
         }
 
-        cout << maxProfit << endl;
+        // Imprime o lucro máximo
+        cout << lucroMaximo << '\n';
     }
 
  return 0;
